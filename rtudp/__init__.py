b7deed6e978f1e90b2c29a629aa151c8147b88af
benf <benjamin.frazer.1997@gmail.com>
2025-09-04 20:28:22 +0000

    added MIT licence and allow pip installation

--- rtudp/__init__.py
+++ rtudp/__init__.py
@@ -1,4 +1,5 @@
 from .rtudp import RtUdp
 from .rtudp_lh import RtUdpLh
 
+__version__ = "0.1.0"
 __all__ = ['RtUdp', 'RtUdpLh']
--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
+import sys
+
+# Platform-specific compile args
+extra_compile_args = ["-O2", "-Wall"]
+extra_link_args = []
+
+if sys.platform == "linux":
+    extra_compile_args.extend(["-pthread", "-fPIC"])
+    extra_link_args.append("-pthread")
 
 rtudp = Extension(
-    name="rtudp.rtudp",  # <- matches your import path!
+    name="rtudp.rtudp",
     sources=["rtudp/rtudp.c"],
-    extra_compile_args=["-g", "-O0"],
-    extra_link_args=["-g"],
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
+    define_macros=[('PY_SSIZE_T_CLEAN', None)],
 )
 
 setup(
     name="rtudp",
-    version="0.0.1",
-    packages=["rtudp"],
+    version="0.1.0",
+    packages=find_packages(),
     ext_modules=[rtudp],
+    package_data={
+        "rtudp": ["*.pyi"],
+    },
+    python_requires=">=3.7",
+    zip_safe=False,
 )
